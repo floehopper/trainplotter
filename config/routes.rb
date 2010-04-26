77533a3c69c505e8bcfa1ac395e4cd1772cd4457
James Mead <james@floehopper.org>
2010-04-26 17:25:08 +0100

    The departures controller was a useful experiment, but we don't need it for now.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :journeys, :collection => { :search => :get, :departing_soon => :get }, :has_many => :positions
-  map.resources :departures, :collection => { :soon => :get }
   map.resources :stations, :collection => { :nearby => :get }
 end
