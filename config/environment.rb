a8e4ff05062fe10809c348126d8b443c3f2b206d
James Mead <james@floehopper.org>
2010-04-28 00:54:27 +0100

    Vendorized rails and unpacked gems.

--- config/environment.rb
+++ config/environment.rb
@@ -20,6 +20,8 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
 
+  config.gem "hpricot"
+  config.gem "mechanize"
   config.gem "geokit"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
