46602faf5b4699ea3b1f7e2f713ce0a9061de51c
James Mead <james@floehopper.org>
2010-04-28 01:19:01 +0100

    Including these gems is causing grief on production, let's take step back.

--- config/environment.rb
+++ config/environment.rb
@@ -20,9 +20,9 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
 
-  config.gem "hpricot"
-  config.gem "mechanize"
-  config.gem "geokit"
+  # config.gem "hpricot"
+  # config.gem "mechanize"
+  # config.gem "geokit"
   config.gem "rack", :version => "1.0.1"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
