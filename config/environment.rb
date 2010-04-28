b1a314aa390ae5c64479594261d1276082112776
James Mead <james@floehopper.org>
2010-04-28 01:11:20 +0100

    Unpacked rack gem.

--- config/environment.rb
+++ config/environment.rb
@@ -23,6 +23,7 @@ Rails::Initializer.run do |config|
   config.gem "hpricot"
   config.gem "mechanize"
   config.gem "geokit"
+  config.gem "rack", :version => "1.0.1"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
