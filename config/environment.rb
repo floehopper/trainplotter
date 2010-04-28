d614651269d1ed475bb288451e63f0ca84598d5f
James Mead <james@floehopper.org>
2010-04-28 11:07:12 +0100

    Added mechanize gem (and its dependency nokogiri) which is used by rake db:seed amongst other things.

--- config/environment.rb
+++ config/environment.rb
@@ -20,9 +20,7 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
 
-  # config.gem "hpricot"
-  # config.gem "mechanize"
-  # config.gem "nokogiri"
+  config.gem "mechanize"
   config.gem "geokit"
   config.gem "rack", :version => "1.0.1"
 
