f9060fe0240f6851da2424f07b7ac77d13f2662c
James Mead <james@floehopper.org>
2010-04-28 11:03:14 +0100

    Add rack > 1.0.1 which seems to be required by rails 2.3.5.

--- config/environment.rb
+++ config/environment.rb
@@ -24,7 +24,7 @@ Rails::Initializer.run do |config|
   # config.gem "mechanize"
   # config.gem "nokogiri"
   # config.gem "geokit"
-  # config.gem "rack", :version => "1.0.1"
+  config.gem "rack", :version => "1.0.1"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
